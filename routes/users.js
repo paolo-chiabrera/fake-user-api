37fafa5722549acadfc47fe6c637e0df47823755
=paolo.chiabrera <=paolo.chiabrera@gmail.com>
2017-06-25 14:16:56 +0200

    Refactor structure

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { name, image, random: { uuid } } = require('faker');
+const { chain, random } = require('lodash');
 
-/* GET users listing. */
+const router = express.Router();
+
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const users = chain(random(1, 10))
+    .range()
+    .map(() => ({
+      firstName: name.firstName(),
+      lastName: name.lastName(),
+      userId: uuid()
+    }))
+    .value();
+
+  res.json(users);
+});
+
+router.get('/:userId', function(req, res, next) {
+  const { userId } = req.params;
+
+  res.json({
+    age: random(18, 70),
+    avatar: image.abstract(),
+    firstName: name.firstName(),
+    jobTitle: name.jobTitle(),
+    lastName: name.lastName(),
+    title: name.title(),
+    userId
+  });
 });
 
 module.exports = router;
